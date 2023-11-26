5fac3168334177d7e6061d6abf9c4813f85f5cc6
Andrey_vdl <81233149+andreyvdl@users.noreply.github.com>
2023-11-25 23:27:39 -0300

    Update options.lua

--- lua/adantas-/core/options.lua
+++ lua/adantas-/core/options.lua
@@ -1,15 +1,29 @@
-vim.opt.clipboard = "unnamedplus"
-vim.opt.completeopt = {"menu", "menuone", "noselect"}
-vim.opt.mouse = "a"
+local options = vim.opt
 
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = false
+options.clipboard = "unnamedplus"
+options.completeopt = {"menu", "menuone", "noselect"}
+options.mouse = "a"
 
-vim.opt.number = true
-vim.opt.cursorline = true
+options.tabstop = 4
+options.softtabstop = 4
+options.shiftwidth = 4
+options.expandtab = false
 
-vim.opt.incsearch = true
-vim.opt.hlsearch = true
-vim.opt.smartcase = true
+options.colorcolumn = "80"
+options.scrolloff = 3
+options.signcolumn = "number"
+options.number = true
+options.cursorline = true
+
+options.incsearch = true
+options.hlsearch = true
+options.smartcase = true
+
+options.encoding = "utf8"
+options.fileencoding = "utf8"
+
+options.syntax = "ON"
+options.termguicolors = true
+
+options.splitright = true
+options.splitbelow = true
