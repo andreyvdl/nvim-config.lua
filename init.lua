8539ec6121da3617f9ef97ead0f961af8e4f7004
Andrey_vdl <81233149+andreyvdl@users.noreply.github.com>
2023-11-25 23:26:25 -0300

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,3 +1,8 @@
+-- [[
+-- tutorials:
+-- https://martinlwx.github.io/en/config-neovim-from-scratch/
+-- https://mattermost.com/blog/how-to-install-and-set-up-neovim-for-code-editing/
+-- ]]
 vim.g.mapleader = " "
 require("adantas-.core.options")
 require("adantas-.core.keymaps")
