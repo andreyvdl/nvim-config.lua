7ea251082d5a7e7a30b821ef4c2b42ba8ca96033
Andrey_vdl <81233149+andreyvdl@users.noreply.github.com>
2023-11-26 21:44:14 -0300

    Update init.lua

--- init.lua
+++ init.lua
@@ -2,8 +2,11 @@
 -- tutorials:
 -- https://martinlwx.github.io/en/config-neovim-from-scratch/
 -- https://mattermost.com/blog/how-to-install-and-set-up-neovim-for-code-editing/
+-- https://github.com/nvim-lua/kickstart.nvim/
 -- ]]
 vim.g.mapleader = " "
-require("adantas-.core.options")
-require("adantas-.core.keymaps")
-require("adantas-.lazy")
+require("adantas-.core.options") -- call configs
+require("adantas-.core.keymaps") -- call keymaps
+require("adantas-.lazy") -- start lazy
+
+-- plugins call
