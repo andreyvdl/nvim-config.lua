60382aa4733e0a2919825edc33314de812d9180c
Andrey_vdl <81233149+andreyvdl@users.noreply.github.com>
2023-12-03 19:00:12 -0300

    Update keymaps.lua

--- lua/adantas-/core/keymaps.lua
+++ lua/adantas-/core/keymaps.lua
@@ -4,7 +4,7 @@ local options = { noremap = true,
 
 local vkeymap = vim.keymap
 
-vkeymap.set("n", "a", "i", options)
+vkeymap.set({"n", "v"}, "a", "i", options)
 vkeymap.set({"n", "v"}, "j", "h", options)
 vkeymap.set({"n", "v"}, "i", "k", options)
 vkeymap.set({"n", "v"}, "k", "j", options)
@@ -18,3 +18,11 @@ vkeymap.set("n", "<M-l>", "<C-w>l", options)
 vkeymap.set("n", "<C-q>", ":q!<CR>", options)
 vkeymap.set("n", "<C-s>", ":w<CR>", options)
 vkeymap.set("n", "<C-x>", ":x<CR>", options)
+
+-- keep visual after identation
+vkeymap.set("v", "<", "<gv")
+vkeymap.set("v", ">", ">gv")
+
+-- change buffer focus
+vkeymap.set("n", "<leader>bn", ":bn<CR>", options)
+vkeymap.set("n", "<leader>bp", ":bp<CR>", options)
