b15ad7da7a10e5436a59b7bf182cbb86e022eed0
Andrey_vdl <81233149+andreyvdl@users.noreply.github.com>
2023-12-14 17:30:04 -0300

    Update plug-cheatsheet.lua

--- lua/adantas-/plugins/plug-cheatsheet.lua
+++ lua/adantas-/plugins/plug-cheatsheet.lua
@@ -1,9 +1,13 @@
-return {
+
 	"sudormrfbin/cheatsheet.nvim",
 	dependencies = {"nvim-telescope/telescope.nvim",
 		"nvim-lua/popup.nvim",
 		"nvim-lua/plenary.nvim",
 	},
 	config = function()
+		local keymap = vim.keymap
+		local opts = {noremap = true, silent = true}
+
+		keymap.set("n", "<leader>help", ":Cheatsheet<CR>", opts)
 	end,
 }
