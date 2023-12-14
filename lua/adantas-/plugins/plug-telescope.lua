31e9e7478a3ec89b64d2886979a3a696c34e9103
Andrey_vdl <81233149+andreyvdl@users.noreply.github.com>
2023-12-14 17:51:19 -0300

    Update plug-telescope.lua

--- lua/adantas-/plugins/plug-telescope.lua
+++ lua/adantas-/plugins/plug-telescope.lua
@@ -2,17 +2,14 @@ return {
 	"nvim-telescope/telescope.nvim",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
-		"BurntSushi/ripgrep",
 		"nvim-tree/nvim-web-devicons",
-		"sharkdp/fd"
 	},
 	config = function()
 		local opts = {noremap = true, silent = true}
 		local keymap = vim.keymap
 		local builtin = require("telescope.builtin")
 
-		keymap.set("n", "<leader>ptf", builtin.find_files, opts)
-		keymap.set("n", "<leader>ptg", builtin.live_grep, opts)
-		keymap.set("n", "<leader>ptb", builtin.buffers, opts)
+		keymap.set("n", "<leader>ff", builtin.find_files, opts)
+		keymap.set("n", "<leader>fb", builtin.buffers, opts)
 	end,
 }
