9873e2a733ffa72961c9c8c979aae1699f3e9bb7
Andrey_vdl <81233149+andreyvdl@users.noreply.github.com>
2023-12-18 23:09:27 -0300

    Update init.lua

--- init.lua
+++ init.lua
@@ -6,6 +6,10 @@ https://github.com/nvim-lua/kickstart.nvim/
 https://youtu.be/ZjMzBd1Dqz8
 --]]
 vim.g.mapleader = " "
+
+-- fix stupid verilog with *.v files
+vim.api.nvim_exec([[autocmd BufNewFile,BufRead *.v setfiletype v]], false)
 require("adantas-.core.options") -- call configs
 require("adantas-.core.keymaps") -- call keymaps
 require("adantas-.lazy") -- start lazy
+
