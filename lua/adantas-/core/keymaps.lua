17a4e1eb49acc49600cac1560d0f962d2a326ffd
Andrey_vdl <81233149+andreyvdl@users.noreply.github.com>
2024-01-13 13:03:09 -0300

    Update keymaps.lua

--- lua/adantas-/core/keymaps.lua
+++ lua/adantas-/core/keymaps.lua
@@ -4,17 +4,6 @@ local options = { noremap = true,
 
 local vkeymap = vim.keymap
 
-vkeymap.set({"n", "v"}, "a", "i", options)
-vkeymap.set({"n", "v"}, "j", "h", options)
-vkeymap.set({"n", "v"}, "i", "k", options)
-vkeymap.set({"n", "v"}, "k", "j", options)
-vkeymap.set({"n", "v"}, "l", "l", options)
-
-vkeymap.set("n", "<M-j>", "<C-w>h", options)
-vkeymap.set("n", "<M-i>", "<C-w>k", options)
-vkeymap.set("n", "<M-k>", "<C-w>j", options)
-vkeymap.set("n", "<M-l>", "<C-w>l", options)
-
 vkeymap.set("n", "<C-q>", ":q!<CR>", options)
 vkeymap.set("n", "<C-s>", ":w<CR>", options)
 vkeymap.set("n", "<C-x>", ":x<CR>", options)
